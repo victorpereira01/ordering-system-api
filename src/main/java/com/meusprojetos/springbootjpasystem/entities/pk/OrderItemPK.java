504d1ea0b6c908692303bbdc01e66441fd9b8544
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-07 17:27:50 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/meusprojetos/springbootjpasystem/entities/pk/OrderItemPK.java
+++ src/main/java/com/meusprojetos/springbootjpasystem/entities/pk/OrderItemPK.java
@@ -24,12 +24,15 @@ public class OrderItemPK implements Serializable {
 	public Order getOrder() {
 		return order;
 	}
+	
 	public void setOrder(Order order) {
 		this.order = order;
 	}
+	
 	public Product getProduct() {
 		return product;
 	}
+	
 	public void setProduct(Product product) {
 		this.product = product;
 	}
