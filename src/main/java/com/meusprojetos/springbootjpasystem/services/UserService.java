6fa26fd43d1d827524f6489b953307166e9aa070
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-07 19:15:15 -0300

    Exception handling - delete

--- src/main/java/com/meusprojetos/springbootjpasystem/services/UserService.java
+++ src/main/java/com/meusprojetos/springbootjpasystem/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.meusprojetos.springbootjpasystem.entities.User;
 import com.meusprojetos.springbootjpasystem.repositories.UserRepository;
+import com.meusprojetos.springbootjpasystem.services.exceptions.DatabaseException;
 import com.meusprojetos.springbootjpasystem.services.exceptions.ResourceNotFoundException;
 
 
@@ -31,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
