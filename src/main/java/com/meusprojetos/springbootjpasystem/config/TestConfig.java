e7681727b1247f5c56d86e23b18279be5c9780a0
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-07 17:39:40 -0300

    Payment, one-to-one association

--- src/main/java/com/meusprojetos/springbootjpasystem/config/TestConfig.java
+++ src/main/java/com/meusprojetos/springbootjpasystem/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.meusprojetos.springbootjpasystem.entities.Category;
 import com.meusprojetos.springbootjpasystem.entities.Order;
 import com.meusprojetos.springbootjpasystem.entities.OrderItem;
+import com.meusprojetos.springbootjpasystem.entities.Payment;
 import com.meusprojetos.springbootjpasystem.entities.Product;
 import com.meusprojetos.springbootjpasystem.entities.User;
 import com.meusprojetos.springbootjpasystem.entities.enums.OrderStatus;
@@ -67,7 +68,7 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+	
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+	
 	}
 
 }
